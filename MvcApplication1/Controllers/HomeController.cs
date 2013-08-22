4028c819f3d46e4a2dfbae5484abfe9a62b868a4
vadzimrudkouski <vadzimrudkouski@gmail.com>
2013-08-22 17:48:43 +0300

    Update HomeController.cs
    test

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace MvcApplication1.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "2Your contact page.";
 
             return View();
         }
